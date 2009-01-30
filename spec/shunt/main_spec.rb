5a0633e553ac8075b88f5474b6c52f33ab2208c8
Otto Hammersmith <otto.hammersmith@gmail.com>
2009-01-29 20:49:08 -0500

    About to nuke some dicking around trying to understand how rspec tests rspecs.

---
+++ spec/shunt/main_spec.rb
@@ -0,0 +1,29 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe "something magical here" do
+  it "should have some rspec pixie dust" do
+    # p self.class
+    # p self.methods.sort
+    # *Spec::Example::ExampleGroup::Subclass_2
+    # ["==", "===", "=~", "__id__", "__send__", "a_kind_of", "allow_message_expectations_on_nil", "an_instance_of", 
+    #   "any_args", "anything", "as_null_object", "backtrace", "be", "be_a", "be_an", "be_close", "boolean", "change", 
+    #   "class", "clone", "context", "describe", "description", "display", "duck_type", "dup", "eql", "eql?", "equal", 
+    #   "equal?", "eval_block", "eval_each_fail_fast", "eval_each_fail_slow", "execute", "exist", "extend", "freeze", 
+    #   "frozen?", "full_description", "has", "hash", "hash_including", "hash_not_including", "have", "have_at_least", 
+    #   "have_at_most", "have_exactly", "id", "implementation_backtrace", "include", "inspect", "instance_eval", "instance_of", 
+    #   "instance_of?", "instance_variable_defined?", "instance_variable_get", "instance_variable_hash", "instance_variable_set", 
+    #   "instance_variables", "is_a?", "kind_of", "kind_of?", "match", "method", "method_missing", "methods", "mock", "nil?", 
+    #   "no_args", "null_object?", "object_id", "options", "pending", "private_methods", "protected_methods", "public_methods", 
+    #   "raise_error", "received_message?", "respond_to", "respond_to?", "rspec_reset", "rspec_verify", "satisfy", "send", 
+    #   "set_instance_variables_from_hash", "setup_mocks_for_rspec", "share_as", "share_examples_for", "shared_examples_for", 
+    #   "should", "should_not", "should_not_receive", "should_receive", "simple_matcher", "singleton_methods", "stub", "stub!", 
+    #   "stub_everything", "subject", "taint", "tainted?", "teardown_mocks_for_rspec", "throw_symbol", "to_a", "to_s", "type", 
+    #   "untaint", "verify_mocks_for_rspec", "violated", "wrap_expectation"]
+    
+    pending
+  end
+end
+
+describe Shunt do
+  
+end
--- spec/shunt_spec.rb
+++ spec/shunt_spec.rb
@@ -1,10 +1,9 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-# Time to add your specs!
-# http://rspec.info/
 describe "Place your specs here" do
   
   it "find this spec in spec directory" do
+    pending "TODO, do I need any top-level specs?"
     violated "Be sure to write your specs"
   end
   
