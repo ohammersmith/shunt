b0901d1b84b3086201f3464af1657f87c88abbdd
Otto Hammersmith <otto.hammersmith@gmail.com>
2009-01-30 00:30:27 -0500

    Got my head on straight, now I see how simple this should have been all along.

--- lib/shunt.rb
+++ lib/shunt.rb
@@ -3,6 +3,31 @@ $:.unshift(File.dirname(__FILE__)) unless
 
 module Shunt
   VERSION = '0.0.1'
+  
+  
+  def go(&proc)
+    @shunt = Shunt.new
+    yield @shunt
+  end
+  
+  class Shunt  # TODO should this be the self construct, WTF is that anyway?
+    def control(&proc)
+      begin
+        proc.call
+      rescue Exception => e
+        @control_exception = e
+      end
+      raise Spec::Expectations::ExpectationNotMetError.new("The code isn't broken, please break something.")
+    end
+    
+    def variable(&proc)
+      begin
+        proc.call
+      rescue Exception => e
+        @variable_exception = e
+      end
+    end
+    
+  end
 end
 
-require 'shunt/main'
--- spec/shunt_spec.rb
+++ spec/shunt_spec.rb
@@ -1,10 +1,35 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 
-describe "Place your specs here" do
-  
-  it "find this spec in spec directory" do
-    pending "TODO, do I need any top-level specs?"
-    violated "Be sure to write your specs"
+describe "Shunt" do
+
+  describe "#go" do
+    
+    it "should result in a failure if both blocks pass" do
+      lambda do
+        
+        Shunt.go do |shunt|
+          shunt.control { true.should be_true }
+          shunt.variable { true.should be_true }
+        end
+        
+      end.should raise_error(Spec::Expectations::ExpectationNotMetError, "The code isn't broken, please break something.") 
+    end
+    
+    it "should result in a failure if only the control block passes" do
+      pending
+    end
+    
+    it "should result in a failure if only the variable block passes" do
+      pending
+    end
+    
+    it "should result in a failure if variable fails in a different way than control" do
+      pending
+    end
+    
+    it "should result in a pending spec if variable fails in exactly the same way as control" do
+      pending
+    end
   end
   
 end
